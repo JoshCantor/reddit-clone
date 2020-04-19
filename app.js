ea702ac3c3a9f5d86fcb6e05ee5375fa04cf69fe
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-19 14:12:49 -0700

    change handleSlashPath to arrow function

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
-function handleSlashPath(req, res) {
-  res.send('Hello World!')
-  // return undefined
-}
+const handleSlashPath = (req, res) => {
+  res.send("Hello World!");
+};
 
 // when someone makes a GET request to '/', call handleSlashPath
-app.get('/', handleSlashPath);
+app.get("/", handleSlashPath);
 
 // takes two args: first is a port number and second is a callback
-app.listen(port, () => console.log(`Reddit clone listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Reddit clone listening at http://localhost:${port}`)
+);
